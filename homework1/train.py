4bb5e9217c63116693945620023202ae1bc2593c
OriginSound <651940260@qq.com>
2023-04-02 10:31:31 +0800

    23/04/02

--- homework1/train.py
+++ homework1/train.py
@@ -1,9 +1,10 @@
 from model import * 
 from dataset import Dataset, DataLoader
-from utils import test 
+from utils import test, plot
 
 import pdb 
 import numpy as np 
+import pickle as pkl 
 
 # Dataset 
 TrainDataloader = DataLoader(Dataset(train=True), batch_size=100)
@@ -11,9 +12,15 @@ TestDataLoader = DataLoader(Dataset(train=False), batch_size=100)
 
 # model and hyper-parameters
 epochs = 20
-lr, decay = 2e-3, 0.05
-gamma = 0.9
+lr, decay = 2e-3, 0.01
+gamma = 0.95
 simpleModel = MyNetworkModel(hidden=200, lr=lr, decay=decay)
+
+# Records 
+train_loss = []
+test_loss = []
+train_accuracy = []
+test_accuracy = []
 
 
 for epoch in range(epochs):
@@ -25,15 +32,29 @@ for epoch in range(epochs):
         simpleModel.forward(data, label)
         simpleModel.step()
 
-    lr, decay = gamma*lr, gamma*decay
+    lr = gamma*lr
     simpleModel.set_train_config(lr=lr, decay=decay)
     
-    # print testing info
+    # record & print testing info
+    loss, accuracy = test(TrainDataloader, simpleModel)
+    train_loss.append(loss)
+    train_accuracy.append(accuracy)
+
     loss, accuracy = test(TestDataLoader, simpleModel)
+    test_loss.append(loss)
+    test_accuracy.append(accuracy)
     print(f"Epoch: {epoch+1:2d}  Loss:{loss:1.4f}  Accuracy: {100*accuracy:3.2f}%")
 
+
+# Save the model and info
 simpleModel.save(f"checkpoint_{epochs}.pkl")
 
+dic = {"train_loss":train_loss, "train_accuracy":train_accuracy,
+        "test_loss": test_loss,  "test_accuracy": test_accuracy}
+with open("loss_accuracy.pkl", "wb") as f:
+    pkl.dump(dic,f)
+
+plot(dic)
 
 
 
--- homework1/utils.py
+++ homework1/utils.py
@@ -18,6 +18,26 @@ def test(Loader, Model):
     return loss_total / num_sample, correct / num_sample
     
 
+def plot(data):
+    x = list(range(len(data["test_loss"])))
+
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+    axs[0].plot(x, data["test_loss"], "o-", linewidth=2.0)
+    axs[0].plot(x, data["train_loss"], "o-", linewidth=2.0)
+    axs[0].set_title("Loss")
+    axs[0].set_xlabel("epoch")
+    axs[0].set_ylabel("loss")
+    axs[0].legend(labels=["Test set", "Train set"])
+
+    axs[1].plot(x, data["test_accuracy"], "o-", linewidth=2.0)
+    axs[1].plot(x, data["train_accuracy"], "o-", linewidth=2.0)
+    axs[1].set_title("Accuracy")
+    axs[1].set_xlabel("epoch")
+    axs[1].set_ylabel("accuracy")
+    axs[1].legend(labels=["Test set", "Train set"])
+    fig.savefig("plot.png")
+    fig.show()
+
 def visualization(Model):
     W1 = Model.W1 
     W2 = Model.W2 
